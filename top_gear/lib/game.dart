4376beae8c66635d584cf620baa6f511771e8187
RicardoJun10r <ricardojunior0110@gmail.com>
2023-10-24 23:35:05 -0300

    att

--- top_gear/lib/game.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flame/events.dart';
-import 'package:flame/game.dart';
-
-class TopGear extends FlameGame with TapCallbacks {}
---
+++ top_gear/lib/game/car_race.dart
@@ -0,0 +1,127 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:async';
+
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:mario_game/game/background.dart';
+import 'package:mario_game/game/managers/managers.dart';
+import 'package:mario_game/game/sprites/competitor.dart';
+import 'package:mario_game/game/sprites/player.dart';
+
+enum Character {
+  bmw,
+  farari,
+  lambo,
+  tarzen,
+  tata,
+  tesla,
+}
+
+class CarRace extends FlameGame
+    with HasKeyboardHandlerComponents, HasCollisionDetection {
+  CarRace({
+    super.children,
+  });
+
+  final BackGround _backGround = BackGround();
+  final GameManager gameManager = GameManager();
+  ObjectManager objectManager = ObjectManager();
+  int screenBufferSpace = 300;
+
+  EnemyPlatform platFrom = EnemyPlatform();
+
+  late Player player;
+
+  @override
+  FutureOr<void> onLoad() async {
+    await add(_backGround);
+    await add(gameManager);
+    overlays.add('gameOverlay');
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (gameManager.isGameOver) {
+      return;
+    }
+    if (gameManager.isIntro) {
+      overlays.add('mainMenuOverlay');
+      return;
+    }
+    // if (gameManager.isPlaying) {
+    //   final Rect worldBounds = Rect.fromLTRB(
+    //     0,
+    //     camera.position.y - screenBufferSpace,
+    //     camera.gameSize.x,
+    //     camera.position.y + _backGround.size.y,
+    //   );
+    //   camera.worldBounds = worldBounds;
+    // }
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 241, 247, 249);
+  }
+
+  void setCharacter() {
+    player = Player(
+      character: gameManager.character,
+      moveLeftRightSpeed: 600,
+    );
+    add(player);
+  }
+
+  void initializeGameStart() {
+    setCharacter();
+
+    gameManager.reset();
+
+    if (children.contains(objectManager)) objectManager.removeFromParent();
+
+    player.reset();
+    // camera.worldBounds = Rect.fromLTRB(
+    //   0,
+    //   -_backGround
+    //       .size.y, // top of screen is 0, so negative is already off screen
+    //   camera.gameSize.x,
+    //   _backGround.size.y +
+    //       screenBufferSpace, // makes sure bottom bound of game is below bottom of screen
+    // );
+    // camera.followComponent(player);
+
+    player.resetPosition();
+
+    objectManager = ObjectManager();
+
+    add(objectManager);
+  }
+
+  void onLose() {
+    gameManager.state = GameState.gameOver;
+    player.removeFromParent();
+    overlays.add('gameOverOverlay');
+  }
+
+  void togglePauseState() {
+    if (paused) {
+      resumeEngine();
+    } else {
+      pauseEngine();
+    }
+  }
+
+  void resetGame() {
+    startGame();
+    overlays.remove('gameOverOverlay');
+  }
+
+  void startGame() {
+    initializeGameStart();
+    gameManager.state = GameState.playing;
+    overlays.remove('mainMenuOverlay');
+  }
+}
